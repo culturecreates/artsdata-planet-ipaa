be638fbb37fb99006b56f5043c51a7cfbef72a81
dev <dev.aravind@culturecreates.com>
2024-01-11 15:30:09 +0530

    Updated sparql to only add derivedFrom to top node

--- tests/add_derived_from_test.rb
+++ tests/add_derived_from_test.rb
@@ -10,11 +10,21 @@ class SparqlTest < Minitest::Test
     @sparql = SPARQL.parse(sparql_file_with_url, update: true)
   end
 
-  def test_derived_from
+  #check the count of derivedFrom triples
+  def test_derived_from_count
     graph = RDF::Graph.load("./fixtures/missing_derived_from.jsonld")
     # puts "before: #{graph.dump(:jsonld)}"
     graph.query(@sparql)
     # puts "after: #{graph.dump(:jsonld)}"
     assert_equal 1,graph.query([nil, RDF::Vocab::PROV.wasDerivedFrom, RDF::URI(@derived_from_url)]).count
+  end
+
+  #test to confirm postal address has no derived from
+  def test_if_postal_address_has_derived_from
+    graph = RDF::Graph.load("./fixtures/missing_derived_from.jsonld")
+    # puts "before: #{graph.dump(:jsonld)}"
+    graph.query(@sparql)
+    # puts "after: #{graph.dump(:jsonld)}"
+    assert_equal nil,graph.query([RDF::URI("http://example.com/test_address"), RDF::Vocab::PROV.wasDerivedFrom, RDF::URI(@derived_from_url)])
   end
 end
