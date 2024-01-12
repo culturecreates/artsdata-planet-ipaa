a03f0c920f7028ea64bcdc9d27c2fb249c9396e6
dev <dev.aravind@culturecreates.com>
2024-01-12 14:03:44 +0530

    added wasDerivedFrom SPARQL to sitemap ruby file

--- sitemap_scraping.rb
+++ sitemap_scraping.rb
@@ -11,23 +11,28 @@ sitemap_url = ARGV[0]
 file_name = ARGV[1]
 
 graph = RDF::Graph.new
-#
+
+add_url_sparql = "./sparqls/add_derived_from.sparql"
+add_url_sparql_file = File.read(add_url_sparql)
 
 sitemap_xml = Nokogiri::XML(URI.open(sitemap_url))
 # Extract all URLs from the sitemap
 ns = { 'xmlns' => 'http://www.sitemaps.org/schemas/sitemap/0.9' }
 entity_urls = sitemap_xml.xpath('//xmlns:url/xmlns:loc', ns).map(&:text)
-puts entity_urls
-entity_urls.each do |entity|
+
+entity_urls.each do |entity_url|
   begin
-    entity = entity.gsub(' ', '+')
-    graph << RDF::Graph.load(entity)
+    entity_url = entity_url.gsub(' ', '+')
+    loaded_graph = RDF::Graph.load(entity_url)
+    sparql_file_with_url = add_url_sparql_file.gsub("subject_url", entity_url)
+    loaded_graph.query(SPARQL.parse(sparql_file_with_url, update: true))
+    graph << loaded_graph
   rescue StandardError => e
     puts "Error loading RDF from #{entity}: #{e.message}"
     break
   end
 end
-#
+
 sparql_file = "./sparqls/replace_blank_nodes.sparql"
 graph.query(SPARQL.parse(File.read(sparql_file), update: true))
 
